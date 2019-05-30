b1b83525607b2b1a4dec9a697612b71c56697ff0
userjack6880 <john@systemanomaly.com>
2019-05-30 16:01:00 -0500

    javascript fun maybe

--- templates/openda/header.php
+++ templates/openda/header.php
@@ -29,6 +29,23 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="templates/openda/style.css" />
+
+		<script>
+			const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+			const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+				v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+				)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+			// do the work...
+			document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+				const table = th.closest('table');
+					Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+						.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+						.forEach(tr => table.appendChild(tr) );
+			})));
+		</script>
+
 		<title><?php page_title(); ?></title>
 	</head>
 	<body>
