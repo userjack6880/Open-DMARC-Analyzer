7a375886d48ebd8b1166f5f8e062974426fe0479
userjack6880 <john@systemanomaly.com>
2019-05-30 16:08:31 -0500

    javascript fun maybe

--- templates/openda/header.php
+++ templates/openda/header.php
@@ -31,19 +31,21 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 		<link rel="stylesheet" type="text/css" href="templates/openda/style.css" />
 
 		<script>
-			const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+			window.onload = function() {
+				const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-			const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-				v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-				)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+				const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+					v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+					)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-			// do the work...
-			document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-				const table = th.closest('table');
-					Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-						.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-						.forEach(tr => table.appendChild(tr) );
-			})));
+				// do the work...
+				document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+					const table = th.closest('table');
+						Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+							.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+							.forEach(tr => table.appendChild(tr) );
+				})));
+			}
 		</script>
 
 		<title><?php page_title(); ?></title>
