c61b6fef98a3757ba4f2f4a6df7ac4def2403de7
userjack6880 <john@systemanomaly.com>
2022-03-16 15:00:44 -0500

    forgot to update a version number

--- includes/template.php
+++ includes/template.php
@@ -23,7 +23,7 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // Versioning
 function oda_version() {
-	echo "0-&alpha;7.2-devPre";
+	echo "0-&alpha;7.2";
 }
 
 // General Page Templates
