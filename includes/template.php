1534f3fe5c88002c94e03640ab9eb61987c12684
userjack6880 <john@systemanomaly.com>
2022-11-29 14:32:40 -0600

    removed dev marker from version for mainline release

--- includes/template.php
+++ includes/template.php
@@ -31,7 +31,7 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 // Versioning -----------------------------------------------------------------
 function oda_version() {
 
-  echo "1-β2 (Dev)";
+  echo "1-β2";
 
 }
 
